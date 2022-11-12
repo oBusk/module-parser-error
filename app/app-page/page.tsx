6d68aa8544fda673a9373667716bb799383c8184
Oscar Busk <oscar.busk@gmail.com>
2022-11-12 23:23:33 +0100

    Add example using node-gyp dependant package

--- app/app-page/page.tsx
+++ app/app-page/page.tsx
@@ -1,3 +1,14 @@
-export default function AppPage() {
-  return <h1>App Page</h1>;
-}
+import pacote from "pacote";
+
+const AppPage = async () => {
+  const manifest = await pacote.manifest("next@latest");
+
+  return (
+    <>
+      <h1>App Page</h1>
+      <pre>{JSON.stringify(manifest, null, 2)}</pre>
+    </>
+  );
+};
+
+export default AppPage;
--- pages/pages-page.tsx
+++ pages/pages-page.tsx
@@ -1,3 +1,27 @@
-export default function PagesPage() {
-  return <h1>Pages Page</h1>;
+import { GetServerSideProps, NextPage } from "next";
+import pacote from "pacote";
+
+interface Props {
+  manifest: pacote.AbbreviatedManifest & pacote.ManifestResult;
 }
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const manifest = await pacote.manifest("next@latest");
+
+  return {
+    props: {
+      manifest,
+    },
+  };
+};
+
+const PagesPage: NextPage<Props> = ({ manifest }) => {
+  return (
+    <>
+      <h1>Pages Page</h1>
+      <pre>{JSON.stringify(manifest, null, 2)}</pre>
+    </>
+  );
+};
+
+export default PagesPage;
